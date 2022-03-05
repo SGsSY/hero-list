85a5f55b5b6d5c284f9069365f0c4d5c83947506
DESKTOP-MM4PQVD\SY <kk12ll55@gmail.com>
2022-03-05 18:13:04 +0800

    project start then redirect to /heroes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {HeroPage} from 'view/HeroPage/HeroPage';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const naviage = useNavigate();
+
+  React.useEffect(() => {
+    naviage('heroes')
+  }, [])
 
   return <HeroPage />
 }
